206e441d1bd14f884d5e7c1f2d99f994db6b8e08
Isabela <isabellmosquera@unicauca.edu.co>
2023-05-03 00:25:42 -0500

    Updated sendRequest function to point doGet method

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function sendRequest() {
     var input = document.getElementById("text").value;
-    console.log("Enviando petición...")
+    console.log("Enviando petición...");
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8080/TechnicalTest/service?operation=getContractData&input=' + input, true);
+    xhr.open('GET', 'http://localhost:8080/TechnicalTest/service?operation=doGet&input=' + input, true);
     xhr.onload = function () {
         if (this.status == 200) {
             let data = JSON.parse(this.responseText).data;
         }
     }
+    xhr.send();
 }
