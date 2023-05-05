525fbf0ada0f2edb930d306ab6306c0f51d533d6
Isabela <isabellmosquera@unicauca.edu.co>
2023-05-04 22:09:09 -0500

    Updated loadContractData function to fix merged cells in table

--- js/script.js
+++ js/script.js
@@ -15,53 +15,70 @@ function sendRequest() {
 function loadContractData(contractData) {
     var tbody = document.getElementById("tbody");
     tbody.innerHTML = " ";
-    console.log(contractData);
 
-    let contractCodes = [];
-    contractData.forEach(function (currentPart) {
-        if (!contractCodes.includes(currentPart.code)) {
-            contractCodes.push(currentPart.code);
-            let partsPerRoles = {
-                'Arrendatario': [],
-                'Propietario': [],
-                'Deudor': []
-            };
-            let contractParts = contractData.filter(function (part) { return part.code == currentPart.code });
-            console.log(contractParts);
-            getPartsPerRole(partsPerRoles, contractParts);
-            let rowspan = Math.max(...Object.values(partsPerRoles).map(function (role) {return role.length}));
+    if (contractData.length == 0) {
+        alert("No exsisten coincidencias. Intente de nuevo.");
+    } else {
+        let contractCodes = [];
+        contractData.forEach(function (currentPart) {
+            if (!contractCodes.includes(currentPart.code)) {
+                contractCodes.push(currentPart.code);
+                let partsPerRoles = {
+                    'Arrendatario': [],
+                    'Propietario': [],
+                    'Deudor': []
+                };
+                let contractParts = contractData.filter(function (part) { return part.code == currentPart.code });
+                console.log(contractParts);
+                getPartsPerRole(partsPerRoles, contractParts);
+                let rowspan = Math.max(...Object.values(partsPerRoles).map(function (role) { return role.length }));
 
-            for (let i = 0; i < rowspan; i++) {
-                let row = document.createElement("tr");
-                if (i == 0) {
-                    var codeCell = document.createElement("td");
-                    codeCell.textContent = contractParts[i].code;
-                    row.appendChild(codeCell);
-                    codeCell.setAttribute("rowspan", rowspan);
+                for (let i = 0; i < rowspan; i++) {
+                    let row = document.createElement("tr");
+                    if (i == 0) {
+                        var codeCell = document.createElement("td");
+                        codeCell.textContent = contractParts[i].code;
+                        row.appendChild(codeCell);
+                        codeCell.setAttribute("rowspan", rowspan);
 
-                    var addressCell = document.createElement("td");
-                    addressCell.textContent = contractParts[i].address;
-                    row.appendChild(addressCell);
-                    addressCell.setAttribute("rowspan", rowspan);
+                        var addressCell = document.createElement("td");
+                        addressCell.textContent = contractParts[i].address;
+                        row.appendChild(addressCell);
+                        addressCell.setAttribute("rowspan", rowspan);
 
-                    var lesseeCell = document.createElement("td");
-                    lesseeCell.textContent = partsPerRoles["Arrendatario"];
-                    row.appendChild(lesseeCell);
-                    lesseeCell.setAttribute("rowspan", rowspan);
+                        var lesseeCell = document.createElement("td");
+                        lesseeCell.textContent = partsPerRoles["Arrendatario"];
+                        row.appendChild(lesseeCell);
+                        lesseeCell.setAttribute("rowspan", rowspan);
+                    }
+
+                    if (i < partsPerRoles["Propietario"].length) {
+                        var ownerCell = document.createElement("td");
+                        ownerCell.textContent = partsPerRoles["Propietario"][i];
+                        if (i == partsPerRoles["Propietario"].length - 1) {
+                            ownerCell.setAttribute("rowspan", rowspan - i)
+                        }
+                        row.appendChild(ownerCell);
+                    }
+
+                    if (i < partsPerRoles["Deudor"].length) {
+                        var debtorCell = document.createElement("td");
+                        debtorCell.textContent = partsPerRoles["Deudor"][i];
+                        if (i == partsPerRoles["Deudor"].length - 1) {
+                            debtorCell.setAttribute("rowspan", rowspan - i)
+                        }
+                        row.appendChild(debtorCell);
+                    } else if (i == 0) {
+                        var debtorCell = document.createElement("td");
+                        debtorCell.setAttribute("rowspan", rowspan)
+                        row.appendChild(debtorCell);
+                    }
+
+                    tbody.appendChild(row);
                 }
-
-                var ownerCell = document.createElement("td");
-                ownerCell.textContent = partsPerRoles["Propietario"][i];
-                row.appendChild(ownerCell);
-
-                var debtorCell = document.createElement("td");
-                debtorCell.textContent = partsPerRoles["Deudor"][i];
-                row.appendChild(debtorCell);
-
-                tbody.appendChild(row);
             }
-        }
-    });
+        });
+    }
 }
 
 function getPartsPerRole(partsPerRoles, contractParts) {
